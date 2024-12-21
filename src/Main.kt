eec231134bd4ab98894d605f669f6f70f55f8c3a
bless tani <blesstani9@gmail.com>
2024-12-21 08:45:38 +0100

    secondary constructor

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,20 @@
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-class Human(name:String){
-    var n:String=name
-    fun think(){
-        println("kotlin is cool $n")
+class Human(n:String){
+
+    var name:String=n
+    var age: Int=0
+    constructor(age: Int, name:String) : this(name){
+        this.age=age;
+    }
+
+    fun express(){
+        println("kotlin is cool $name : $age")
     }
 }
 fun main() {
-    val ben= Human("ben")
-    ben.think()
+    val ben= Human(40,"Ben")
+    ben.express()
 
 }
